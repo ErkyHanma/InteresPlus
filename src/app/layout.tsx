0ab82e452839b7624cc83e5990f86d1e63af9cc3
Erkyhanma <yenzel.bt@gmail.com>
2026-03-23 00:21:31 -0400

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "InteresPlus",
   description: "Calculadora de Interes Simple & Compuesto",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
